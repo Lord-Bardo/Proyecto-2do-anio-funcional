771e6542d85d5335c586e37ef4737176d834414b
Lord-Bardo <mlombardo@frba.utn.edu.ar>
2023-05-21 10:26:28 -0300

    HAY QUE MEJORAR ESTO ANTES DE SEGUIR

--- src/Library.hs
+++ src/Library.hs
@@ -5,6 +5,10 @@ import GHC.Conc (setAllocationCounter)
 import GHC.Num (Num)
 
 
+data Cabezal = Cabezal {
+    fila:: Number,
+    col :: Number
+}deriving (Eq, Show)
 
 
 data Bolita = Rojo | Azul | Verde | Negro  deriving (Eq, Show)
@@ -20,11 +24,11 @@ data Direccion = Norte | Sur | Este | Oeste deriving (Eq, Show)
 
 
 
--- moverDireccion :: Direccion -> [Number] ->[Number] --Hacer verificacion de que existe la c-1 /c+1 etc creo que mejor tupla el cabezal
--- moverDireccion Norte (f:c:fs) = (f-1:c:fs)
--- moverDireccion Sur (f:c:fs)=(f+1:c:fs)
--- moverDireccion Este (f:c:fs)=(f:c+1:fs)
--- moverDireccion Oeste (f:c:fs)=(f:c-1:fs)
+moverDireccion :: Direccion -> Cabezal ->Cabezal --Hacer verificacion de que existe la c-1 /c+1 etc creo que mejor tupla el cabezal
+moverDireccion Norte  cabezal= Cabezal ((fila cabezal) -1) (col cabezal)
+moverDireccion Sur cabezal= Cabezal ((fila cabezal) +1) (col cabezal)
+moverDireccion Este cabezal= Cabezal (fila cabezal) ((col cabezal) +1)
+moverDireccion Oeste cabezal= Cabezal (fila cabezal) ((col cabezal) -1)
 
 -- type Espacio =((Bolita, Number),(Bolita, Number),(Bolita, Number),(Bolita, Number))
 
@@ -45,16 +49,30 @@ inicializar :: Number -> Number -> [[Espacio]]
 inicializar filas col
     | col==1 = [generarFilas filas]
     | otherwise = generarFilas filas : inicializar filas (col -1)
+cabezal = Cabezal 0 0
+
 
 -- sumarBolita :: [[Espacio]] -> Number->Number -> Bolita->[[Espacio]]
 -- sumarBolita tablero fila col color
 --     | 
+
+
 sumarBolita :: [Espacio] -> Number-> Number -> Bolita->[Espacio]
 sumarBolita [] _ _ _ = []
 sumarBolita lista fila posicion color
     |   posicion == length lista = [queDevuelvo lista fila posicion color]
     |   otherwise = queDevuelvo lista fila posicion color : sumarBolita (tail lista) fila (posicion+1) color
 
+ponerBola :: [[Espacio]] ->Cabezal->Number ->Bolita ->[[Espacio]] -- Primera posicion va  a ser 0
+ponerBola [] _ _ _= []
+ponerBola tablero cabezal posicion color
+    |   1 == length tablero = [temporal tablero (fila cabezal) (col cabezal) posicion color]
+    |   otherwise = temporal tablero (fila cabezal) (col cabezal) posicion color : ponerBola (tail tablero) cabezal (posicion +1) color
+
+temporal :: [[Espacio]]-> Number->Number ->Number->Bolita ->[Espacio] -- Ultima posicion es col-1
+temporal tablero fila col posicion color
+    |   col == posicion = sumarBolita (head tablero) fila 0 color
+    |   otherwise = head tablero
 
 queDevuelvo :: [Espacio] -> Number -> Number -> Bolita ->Espacio
 queDevuelvo lista fila posicion color
